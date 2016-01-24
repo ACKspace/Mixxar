5b4d9a98e79148813ca5e707c2aa26146ff59426
computer1up <redoo1@kpnmail.nl>
2016-01-24 20:07:16 +0100

    kjfdla;k

--- main.py
+++ main.py
@@ -5,18 +5,22 @@ os.system("cls")
 #initialize the processes and volume for each slider using dictionaries
 program = {"A": "UT2004.exe", "B": "chrome.exe", "C": "telegram.exe", "D": "firefox.exe"}
 setVolume = {"A": 0, "B": 0, "C": 0, "D": 0}
+ser = ""
 
 def ardConnectionCheck():
+    global ser
     #cycle through all available COM ports to poll for a connected device
     #repeats itself if no arduino is found on the COM ports
     print "Connecting to arduino..."
     ports = ['COM%s' % (i + 1) for i in range(256)]
+    
+    ser = serial.Serial(port, 9600, timeout=2)
     ardFound = False
     while not ardFound:
         for port in ports:
             print "Testing %s" %(port)
             try:
-                ser = serial.Serial(port, 9600, timeout=2)
+                
                 serInput = ser.readline()
                 #read only the first 13 characters, as a weird character is being output at the end
                 serInput = serInput[0:13]
@@ -34,7 +38,9 @@ def ardConnectionCheck():
                     break
             except (OSError, serial.SerialException):
                 pass        
-        
+            #reads connection-ok from every line
+            serConnection = ser.readline()
+            ardCon = serConnection[0:6]
 
 #return ready state to the arduino to begin slider configuration
 #ser.write("OS READY")
@@ -43,23 +49,25 @@ def ardConnectionCheck():
 
 #main loop
 def volChange():
+    global ser
+    global ardCon
     while 1:
         #read the serial output and the slider for which the output is given
         serialreading = ser.readline()
-        serialVolume = float(serialreading[6:])/1024.0
+        serialVolume = float(serialreading[7:])/1024.0
         slider = serialreading[0]
-        #reads connection-ok from every line
-        serConnection = ser.readline()
-        ardCon = serConnection[0:5]    
-    #checks if the arduinos connection is still intact
-    while ardCon == "CON-OK":
-        #set new volume if the slider has changed, constantly adjusting is more resource-heavy
-        if serialVolume != setVolume[slider]:
-            os.system("nircmd setappvolume %s %f" %(program[slider], serialVolume))
-            setVolume[slider] = serialVolume
-    #if arduino is disconnected, restart the connection-initializer script
-    else: 
-        ardConnectionCheck()
+        #connection check
+        while ardCon == "CON-OK":
+            #set new volume if the slider has changed, constantly adjusting is more resource-heavy
+            if serialVolume != setVolume[slider]:
+                os.system("nircmd setappvolume %s %f" %(program[slider], serialVolume))
+                setVolume[slider] = serialVolume
+            #if arduino is disconnected, restart the connection-initializer script
+            else: 
+                ardConnectionCheck()        
+
+
+    
         
 #starts the progam
 ardConnectionCheck()
